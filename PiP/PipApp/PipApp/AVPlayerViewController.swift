94582e6777649d9c6d0a2381e73aa5276aa078ca
Sedat <sedatdlkrn46@gmail.com>
2022-09-05 09:32:21 +0300

    PiP

---
+++ PiP/PipApp/PipApp/AVPlayerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AVPlayerViewController.swift
+//  PipApp
+//
+//  Created by SEDAT DALKIRAN on 4.08.2022.
+//
+
+import UIKit
+import AVKit
+
+class AVPlayerViewController: AVPlayerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
